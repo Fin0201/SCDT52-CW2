d102d5bf8f436582f06b012544bb8bf848d7be4c
Fin0201 <edw17006550@btc.ac.uk>
2023-01-16 15:12:49 +0000

    Added a favicon

--- components/reg-form.php
+++ components/reg-form.php
@@ -24,6 +24,7 @@
                'password' => password_hash($password['value'], PASSWORD_DEFAULT)];
 
       $member = $controllers->members()->register_member($args);
+      echo($member);
       if ($member) {
         redirect("login", ["error" => "Please login with your new account"]);
       } else {
